e322aca8a59bff8f0307b5f5fc819c62b4aedc7f
Kássio Silva <Kassio.Sa@ibm.com>
2023-05-02 11:45:08 -0300

    feat: Add validações nos campos

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -22,15 +22,27 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
        }
     }
 
+    private fun isValid(): Boolean {
+        return (binding.editDistance.text.toString() != ""
+                && binding.editPrice.text.toString() != ""
+                && binding.editAutonomy.text.toString() != ""
+                && binding.editAutonomy.text.toString().toFloat() != 0f)
+    }
+
     private fun calculate() {
-        val distance = binding.editDistance.text.toString().toFloat()
-        val price = binding.editPrice.text.toString().toFloat()
-        val autonomy = binding.editAutonomy.text.toString().toFloat()
+        if (isValid()) {
+            val distance = binding.editDistance.text.toString().toFloat()
+            val price = binding.editPrice.text.toString().toFloat()
+            val autonomy = binding.editAutonomy.text.toString().toFloat()
 
-        val totalValue = (distance * price) / autonomy
-        val totalValueFormatted = "R$ ${"%.2f".format(totalValue)}"
+            val totalValue = (distance * price) / autonomy
+            val totalValueFormatted = "R$ ${"%.2f".format(totalValue)}"
 
-        binding.textTotalValue.text = totalValueFormatted
+            binding.textTotalValue.text = totalValueFormatted
+        } else {
+            Toast.makeText(this, R.string.validation_fill_all_fields, Toast.LENGTH_LONG).show()
+        }
+
 
         // Toast.makeText(this, totalValueStr, Toast.LENGTH_LONG).show()
     }
